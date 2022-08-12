3072d2271cd6e51716410161c9a97255f747396f
Joaquin Lopez <92270610+JoaquinLoud@users.noreply.github.com>
2022-08-12 10:40:13 -0500

    modifique el ejercicio de BD

--- joauqin.py
+++ joauqin.py
@@ -1,2 +1,8 @@
-print("")
-print("Joaquin ")
+edad = 0
+    
+while edad <18: 
+    edad = edad + 1
+    if edad ==1:
+        print (f"Felicidades, tienes  {edad} año") 
+    else:
+        print (f"Felicidades, tienes {edad} años")
